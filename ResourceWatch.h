49c8f119877f5207d9bff09dc4e72fdb4668d9c3
Filipp Pavlov <filipp@ccpgames.com>
2017-09-15 13:01:39 +0000

    blue: added a resource watcher class
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/blue/": change = 1188331]

---
+++ ResourceWatch.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Include/BlueAsyncRes.h"
+
+BLUE_CLASS( ResourceWatch ): public IRoot, IBlueAsyncResNotifyTarget
+{
+public:
+	EXPOSE_TO_BLUE();
+
+	ResourceWatch( IRoot* lockobj = nullptr );
+	~ResourceWatch();
+
+	void Watch( BlueAsyncRes* resource );
+protected:
+	virtual void ReleaseCachedData( BlueAsyncRes* resource );
+	virtual void RebuildCachedData( BlueAsyncRes* resource );
+private:
+	BlueWeakRef<BlueAsyncRes> m_resource;
+	BlueScriptCallback m_onLoaded;
+	BlueScriptCallback m_onUnloaded;
+};
+
+TYPEDEF_BLUECLASS( ResourceWatch );
