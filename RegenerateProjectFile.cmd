776285a08fd6aa78812ae72989714c75dbd4583b
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2019-04-08 10:11:33 +0000

    carbon/src/blue: VS2017 Support: Set "TreatWarningsAsError" to false (to circumvent the Windows API "GetVersion" deprecation warning), and add _WINSOCK_DEPRECATED_NO_WARNINGS.
    ..and add an initial "blue_v141.vcxproj" file.
    
    Also, comment out some obsolete-seeming "v110_xp" and "orbis" stuff in "RegerateProjectFile.cmd". 
    
    (via partial merge from RNB-MISC@1476921)
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/blue/": change = 1487129]

--- RegenerateProjectFile.cmd
+++ RegenerateProjectFile.cmd
@@ -2,12 +2,12 @@
 echo Checking out project and filters file
 p4 edit blue.vcxproj
 p4 edit blue.vcxproj.filters
-p4 edit blue.v110_xp.vcxproj
-p4 edit blue.v110_xp.vcxproj.filters
-p4 edit blue.orbis.vcxproj
-p4 edit blue.orbis.vcxproj.filters
+REM p4 edit blue.v110_xp.vcxproj
+REM p4 edit blue.v110_xp.vcxproj.filters
+REM p4 edit blue.orbis.vcxproj
+REM p4 edit blue.orbis.vcxproj.filters
 echo Regenerating
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --toolset=v100
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --toolset=v110_xp
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --orbis
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj
+REM ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --toolset=v110_xp
+REM ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --orbis
 pause
