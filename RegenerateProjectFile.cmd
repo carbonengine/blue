8f5fe7c8ac74cea38baf1baa613d20fe87f2dbd3
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2020-05-15 17:31:16 +0000

    Merge using //eve/development/MAINLINE->//eve/sandbox/PER-BRANCH-SDK-FOLDER
    This brings in the vendored SDK work
    https://jira.ccpgames.com/browse/TTC-1025
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/blue/": change = 1727524]

--- RegenerateProjectFile.cmd
+++ RegenerateProjectFile.cmd
@@ -1,13 +1,7 @@
 @echo off
 echo Checking out project and filters file
-p4 edit blue.vcxproj
-p4 edit blue.vcxproj.filters
-REM p4 edit blue.v110_xp.vcxproj
-REM p4 edit blue.v110_xp.vcxproj.filters
-REM p4 edit blue.orbis.vcxproj
-REM p4 edit blue.orbis.vcxproj.filters
+p4 edit blue_v141.vcxproj
+p4 edit blue_v141.vcxproj.filters
 echo Regenerating
-..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj
-REM ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --toolset=v110_xp
-REM ..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --orbis
+..\..\..\..\..\..\shared_tools\python\27\python.exe ..\..\tools\ProjectFileGenerator\ProjectFileGenerator.py -i blue.ccpproj --toolset=v141 --outfile=blue_v141.vcxproj
 pause
