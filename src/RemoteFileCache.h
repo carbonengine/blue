bb1463341b62b1ed8dbd4873909210dcc6c20d20
CCP Cookies <69311849+ccpcookies@users.noreply.github.com>
2023-05-18 12:15:09 +0000

    Prepare client for migration to R2
    Changes include:
    Feature flag to change primary server implemented
    Exception no called when primary server fails
    Feature flag to supress said exception
    Ability for client to decide to fallback to backup server as default after certain sequetial fails from primary server
    Feature flag to specify said primary server fail fallback threshold

--- src/RemoteFileCache.h
+++ src/RemoteFileCache.h
@@ -9,7 +9,7 @@
 #ifndef RemoteFileCache_h
 #define RemoteFileCache_h
 
-#include "IBluePersist.h"
+#include "Include/IBluePersist.h"
 
 extern double g_thresholdForWarningLongDownloadTime;
 extern double g_thresholdForAbortingLongDownloadTime;
@@ -37,11 +37,16 @@ public:
 	bool IsDirectory( const wchar_t* resPath );
 	Be::Result<std::string> ListDir( const wchar_t* resPath, std::list<std::string>& contents );
 
+	void RegisterOnServerFailedCallback( BlueScriptCallback callback );
+
 private:
 	std::string m_server;
 	std::string m_backupServer;
 	std::string m_prefix;
 	std::wstring m_cacheFolder;
+	bool m_registerDownloadErrors;
+	int m_numSequentialPrimaryServerDownloadErrors;
+	int m_primaryServerFailThreshold;
 
 	struct FileInfo
 	{
@@ -66,6 +71,8 @@ private:
 	bool m_fullHeaderLogging;
 	bool m_verifyContentsOnSave;
 	bool m_verifyContentsOnLoad;
+
+	BlueScriptCallback m_on_server_failed_callback;
 
 private:
 	bool AddFileIndexFromStream( IBlueStream* stream );
