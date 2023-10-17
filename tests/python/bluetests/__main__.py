6bcad4737e60a8a6a7b63e0c49f491eb1691d816
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2023-10-17 14:01:13 +0000

    Add ability to run `bluetests` via `-m`
    This is required for tests which depend on our customized socket module (e.g. anything involving network functionality). The `unittest` module imports `socket` and thus we can no longer monkeypatch it. With this functionality we have a chance to monkeypatch the socket module in order to run the tests. Furthermore, we can run `unittest.main()` on a tasklet, allowing tests to behave transparently asynchronous, like the rest of the Python codebase.
    The implementation is largely inspired by `unittest`'s own `__main__.py` module.

---
+++ tests/python/bluetests/__main__.py
@@ -0,0 +1,25 @@
+def main():
+    import sys
+    if sys.argv[0].endswith("__main__.py"):
+        import os.path
+        # We change sys.argv[0] to make help message more useful
+        # use executable without path, unquoted
+        # (it's just a hint anyway)
+        # (if you have spaces in your executable you get what you deserve!)
+        executable = os.path.basename(sys.executable)
+        sys.argv[0] = executable + " -m unittest"
+        del os
+    import carbonio
+    import _slsocket
+    _slsocket.use_carbonio(True)
+    carbonio._socket = _slsocket
+    sys.modules["_socket"] = _slsocket
+    import stackless
+    # Cannot import unittest before we patch the socket, because unittest imports socket.
+    from unittest import main
+    __unittest = True
+    t = stackless.tasklet(main)(module=None)
+    while t.alive:
+        stackless.run()
+
+main()
