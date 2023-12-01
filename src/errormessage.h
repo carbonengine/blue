67de6b6bd1a8d4e005d2214153afac3dfeb2f707
CCP Cookies <69311849+ccpcookies@users.noreply.github.com>
2023-12-01 14:57:28 +0000

    Expose blue error messages so they can be used external to blue
    Work done to allow use of early messaging system in exefile.
    Relates to: https://ccpgames.atlassian.net/browse/PLAT-2737

--- src/errormessage.h
+++ src/errormessage.h
@@ -1,6 +1,13 @@
 #ifndef _ERRORMESSAGE_H_
 #define _ERRORMESSAGE_H_
 
+////////////////////////////////////////////////////////////
+//
+//  Created:		Feb 2021
+//  Copyright:		CCP 2023
+//  Documentation:	https://ccpgames.atlassian.net/wiki/spaces/TC/pages/177930592/Localizing+Early+Error+Messages
+//
+
 #include <string>
 
 #if __APPLE__
@@ -9,11 +16,29 @@
 
 #define IDS_VERIFYFAIL_M 106
 #define IDS_VERIFYFAIL_C 107
-#define IDS_INVALIDWINDOWS 108
+#define IDS_INVALIDOS 108
 #define IDS_VERIFYFAIL_M1 109
 #define IDS_VERIFYFAIL_NOTFOUND 110
 #define IDS_VERIFYFAIL_UNKNOWNFOUND 111
 #define IDS_VERIFYFAIL_INCORRECTCRC 112
+
+struct BlueErrorMessage
+{
+	std::string de;
+	std::string en;
+	std::string es;
+	std::string fr;
+	std::string ja;
+	std::string ko;
+	std::string ru;
+	std::string zh;
+};
+
+// Exposed to allow altering of error message at runtime, useful for altertering message based on system spec.
+extern "C" BLUEIMPORT void BlueSetErrorMessage(const BlueErrorMessage & message, unsigned int messageId);
+
+// Exposed so that dialog can be requested externally to blue
+extern "C" BLUEIMPORT void BlueShowMessageBox(const std::string & title, unsigned int titleId, const std::string & message, unsigned int messageId);
 
 // Translate an error message to an appropriate language for the user - returns the original string on failure.
 std::string TranslateErrorMessage( const std::string& original, unsigned messageID );
