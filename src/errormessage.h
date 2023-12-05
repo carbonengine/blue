0d9d78854751893f7ae77d7b07ded810d08538e6
CCP Cookies <69311849+ccpcookies@users.noreply.github.com>
2023-12-05 15:07:49 +0000

    Expose new error message to show invalid os strings
    Required to be called from carbon-exefile when detecting
    unsupported operating systems

--- src/errormessage.h
+++ src/errormessage.h
@@ -16,29 +16,16 @@
 
 #define IDS_VERIFYFAIL_M 106
 #define IDS_VERIFYFAIL_C 107
-#define IDS_INVALIDOS 108
+#define IDS_INVALIDWINDOWS 108
 #define IDS_VERIFYFAIL_M1 109
 #define IDS_VERIFYFAIL_NOTFOUND 110
 #define IDS_VERIFYFAIL_UNKNOWNFOUND 111
 #define IDS_VERIFYFAIL_INCORRECTCRC 112
+#define IDS_INVALIDMACOS 113
+#define IDS_INVALIDOS 114
 
-struct BlueErrorMessage
-{
-	std::string de;
-	std::string en;
-	std::string es;
-	std::string fr;
-	std::string ja;
-	std::string ko;
-	std::string ru;
-	std::string zh;
-};
-
-// Exposed to allow altering of error message at runtime, useful for altertering message based on system spec.
-extern "C" BLUEIMPORT void BlueSetErrorMessage(const BlueErrorMessage & message, unsigned int messageId);
-
-// Exposed so that dialog can be requested externally to blue
-extern "C" BLUEIMPORT void BlueShowMessageBox(const std::string & title, unsigned int titleId, const std::string & message, unsigned int messageId);
+// Exposed so invalid os dialogs can be requested externally to blue
+extern "C" BLUEIMPORT void BlueShowInvalidOSVersionError();
 
 // Translate an error message to an appropriate language for the user - returns the original string on failure.
 std::string TranslateErrorMessage( const std::string& original, unsigned messageID );
