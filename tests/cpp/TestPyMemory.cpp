bbc6bfd650f1edceccf0ba88e74df61c2bea6639
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2025-06-13 13:02:01 +0000

    Wrap Python's default allocator
    Instead of using carbon-core's memory allocation functions. The reason here is that Python itself uses a very sophisticated allocator. Also, contrary to the initial investigation, our customized version of Stackless Python was not simply using the memory allocation functions from carbon-core. This was very difficult to track down due to the complexity of the stackless modification.
    
    Alas, this change now simply wraps Python's default allocators to track the amount of memory currently allocated by Python.

--- tests/cpp/TestPyMemory.cpp
+++ tests/cpp/TestPyMemory.cpp
@@ -59,27 +59,24 @@ TEST_F(PythonMemoryAllocatorTest, GetInstalledCorrectly)
     PyMemAllocatorEx observed;
     PyMem_GetAllocator( PYMEM_DOMAIN_RAW, &observed );
     ASSERT_NE( observed.ctx, nullptr );
-    EXPECT_EQ( observed.malloc, &Ccp::PyRawMalloc );
-    EXPECT_EQ( observed.calloc, &Ccp::PyRawCalloc );
-    EXPECT_EQ( observed.realloc, &Ccp::PyRawRealloc );
-    EXPECT_EQ( observed.free, &Ccp::PyRawFree );
+    EXPECT_EQ( observed.malloc, &Ccp::MeasuredMalloc );
+    EXPECT_EQ( observed.calloc, &Ccp::MeasuredCalloc );
+    EXPECT_EQ( observed.realloc, &Ccp::MeasuredRealloc );
+    EXPECT_EQ( observed.free, &Ccp::MeasuredFree );
 
-    // mem and obj allocators require that the GIL is held, see:
-    // https://docs.python.org/3/c-api/memory.html#memory-interface
-    // https://docs.python.org/3/c-api/memory.html#object-allocators
     PyMem_GetAllocator( PYMEM_DOMAIN_MEM, &observed );
     ASSERT_NE( observed.ctx, nullptr );
-    EXPECT_EQ( observed.malloc, &Ccp::PyMallocWithGIL );
-    EXPECT_EQ( observed.calloc, &Ccp::PyCallocWithGIL );
-    EXPECT_EQ( observed.realloc, &Ccp::PyReallocWithGIL );
-    EXPECT_EQ( observed.free, &Ccp::PyFreeWithGIL );
+	EXPECT_EQ( observed.malloc, &Ccp::MeasuredMalloc );
+	EXPECT_EQ( observed.calloc, &Ccp::MeasuredCalloc );
+	EXPECT_EQ( observed.realloc, &Ccp::MeasuredRealloc );
+	EXPECT_EQ( observed.free, &Ccp::MeasuredFree );
 
     PyMem_GetAllocator( PYMEM_DOMAIN_OBJ, &observed );
     ASSERT_NE( observed.ctx, nullptr );
-    EXPECT_EQ( observed.malloc, &Ccp::PyMallocWithGIL );
-    EXPECT_EQ( observed.calloc, &Ccp::PyCallocWithGIL );
-    EXPECT_EQ( observed.realloc, &Ccp::PyReallocWithGIL );
-    EXPECT_EQ( observed.free, &Ccp::PyFreeWithGIL );
+	EXPECT_EQ( observed.malloc, &Ccp::MeasuredMalloc );
+	EXPECT_EQ( observed.calloc, &Ccp::MeasuredCalloc );
+	EXPECT_EQ( observed.realloc, &Ccp::MeasuredRealloc );
+	EXPECT_EQ( observed.free, &Ccp::MeasuredFree );
 }
 
 TEST_F(PythonMemoryAllocatorTest, TracksAllocatedAmount)
