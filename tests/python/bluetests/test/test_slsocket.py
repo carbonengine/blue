1fc777f51ca44df920bdf7222bc597bea13d24d2
Hrafn Jökull Geirsson <5185245+hrafn@users.noreply.github.com>
2025-09-11 16:24:49 +0000

    Fix up slsocket unit tests to run on macOS
    carbonio simply does not exist on macOS, so skip any related tests. Fix up StacklessIO test that expected all the data for a packet to get sent at once, but the header got received separately, ahead of the body.

--- tests/python/bluetests/test/test_slsocket.py
+++ tests/python/bluetests/test/test_slsocket.py
@@ -10,6 +10,7 @@ LOCALHOST = "127.0.0.1"
 
 def find_port():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         s.bind((LOCALHOST, 0))
         port = s.getsockname()[1]
@@ -17,21 +18,23 @@ def find_port():
     finally:
         s.close()
 
-def run_on_server(host_address, function):
+def run_on_server(channel, function):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         server.setmaxpacketsize(10 * 1024 * 1024)
-        server.bind(host_address)
+        server.bind(("127.0.0.1", 0))
+        channel.send(server.getsockname())
         server.listen(0)
         connection, address = server.accept()
         function(connection)
     finally:
         server.close()
 
-def run_on_client(host_address, function):
+def run_on_client(channel, function):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
+        host_address = channel.receive()
         client.connect(host_address)
         client.setmaxpacketsize(10 * 1024 * 1024)
         client.setblockingsend(True)
@@ -56,18 +59,32 @@ def receive(connection):
     receive.received = connection.recv(65535)
 receive.received = None
 
+def receive_bytes(byte_count, connection):
+    receive_bytes.received = ""
+    while len(receive_bytes.received) < byte_count:
+        data = connection.recv(byte_count - len(receive_bytes.received))
+        receive_bytes.received += data
+receive_bytes.received = None
+
 def run_connected(client_func, server_func):
-    port = find_port()
-    address = (LOCALHOST, port)
-    c = stackless.tasklet(run_on_client)(address, client_func)
-    s = stackless.tasklet(run_on_server)(address, server_func)
-    while c.alive or s.alive:
-        blue.os.Pump()
+    channel = stackless.channel()
+    c = stackless.tasklet(run_on_client)(channel, client_func)
+    s = stackless.tasklet(run_on_server)(channel, server_func)
+    sleeptime = blue.os.sleeptime
+    try:
+        blue.os.sleeptime = 100
+        while c.alive or s.alive:
+            blue.os.Pump()
+    finally:
+        blue.os.sleeptime = sleeptime
 
 class TestCarbonIO(unittest.TestCase):
 
     def setUp(self):
-        import carbonio
+        try:
+            import carbonio
+        except ImportError:
+            raise unittest.SkipTest("CarbonIO not supported on this system")
         import _slsocket
         _slsocket.use_carbonio(True)
         carbonio._socket = _slsocket
@@ -110,7 +127,8 @@ class TestStacklessIO(unittest.TestCase):
         import stacklessio
         import _slsocket
         import slselect
-        _slsocket.use_carbonio(False)
+        if hasattr(_slsocket, "use_carbonio"):
+            _slsocket.use_carbonio(False)
         stacklessio._socket = _slsocket
         sys.modules["_socket"] = _slsocket
         sys.modules["select"] = slselect
@@ -125,8 +143,8 @@ class TestStacklessIO(unittest.TestCase):
     def test_sent_packet_format(self):
         PACKET_DATA = "data"
         OOB_DATA = None
-        run_connected(functools.partial(send_packet, PACKET_DATA, OOB_DATA), receive)
-        self.assertEqual(receive.received, "\x00\x00\x00\x04data")
+        run_connected(functools.partial(send_packet, PACKET_DATA, OOB_DATA), functools.partial(receive_bytes, 8))
+        self.assertEqual(receive_bytes.received, "\x00\x00\x00\x04data")
 
     def test_receive_formatted_packet(self):
         PACKET_DATA = "\x00\x00\x00\x04data"
